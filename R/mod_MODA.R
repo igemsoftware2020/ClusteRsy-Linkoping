817a71ffa4e0cdbe0347a72e0257adba7b1a86db
Hyperty <56047053+Hypertyz@users.noreply.github.com>
2020-06-15 16:52:21 +0200

    Creat MCODE and MODA modules

---
+++ R/mod_MODA.R
@@ -0,0 +1,51 @@
+#' MODA UI Function
+#'
+#' @description A shiny Module.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd 
+#'
+#' @importFrom shiny NS tagList 
+mod_MODA_ui <- function(id){
+  ns <- NS(id)
+  tagList(
+    uiOutput(ns("input_choice")),
+    uiOutput(ns("ppi_choice")),
+    textInput(ns("module_name"), "Module object name"),
+    actionButton(ns("load_input"), "Infer MODA module"),
+  )
+}
+    
+#' MODA Server Function
+#'
+#' @noRd 
+mod_MODA_server <- function(input, output, session){
+  ns <- session$ns
+ 
+  output$input_choice <- renderUI({
+    input_objects <- unlist(MODifieRDB::get_available_input_objects(con)$input_name)
+    selectInput(ns("input_object"), label = "Input object", choices = input_objects)
+  })
+  
+  output$ppi_choice <- renderUI({
+    ppi_networks <- unlist(MODifieRDB::get_available_networks(con))
+    selectInput(ns("ppi_object"), label = "PPI network", choices = ppi_networks)
+  })
+  observeEvent(input$load_input, {
+    module_object <- MODifieRDB::mcode_db(input_name = input$input_object, 
+                                          ppi_name = input$ppi_object, 
+                                          deg_cutoff = .98, 
+                                          module_name = input$module_name,
+                                          con = con)
+    
+    
+  })
+}
+    
+## To be copied in the UI
+# mod_MODA_ui("MODA_ui_1")
+    
+## To be copied in the server
+# callModule(mod_MODA_server, "MODA_ui_1")
+ 
