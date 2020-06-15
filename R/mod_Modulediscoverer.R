42866987d63992a8e31b8bdf07387d53774d0c46
Lucas Porcile <lucasporcile@Lucass-MacBook-Pro.local>
2020-06-15 13:45:49 +0200

    Added Module discoverer

---
+++ R/mod_Modulediscoverer.R
@@ -0,0 +1,52 @@
+#' Modulediscoverer UI Function
+#'
+#' @description A shiny Module.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd 
+#'
+#' @importFrom shiny NS tagList 
+mod_Modulediscoverer_ui <- function(id){
+  ns <- NS(id)
+  tagList(
+    uiOutput(ns("input_choice")),
+    uiOutput(ns("ppi_choice")),
+    textInput(ns("module_name"), "Module object name"),
+    actionButton(ns("load_input"), "Infer Module discoverer module")
+ 
+  )
+}
+    
+#' Modulediscoverer Server Function
+#'
+#' @noRd 
+mod_Modulediscoverer_server <- function(input, output, session){
+  ns <- session$ns
+  output$input_choice <- renderUI({
+    input_objects <- unlist(MODifieRDB::get_available_input_objects(con)$input_name)
+    selectInput(ns("input_object"), label = "Input object", choices = input_objects)
+  })
+  
+  output$ppi_choice <- renderUI({
+    ppi_networks <- unlist(MODifieRDB::get_available_networks(con))
+    selectInput(ns("ppi_object"), label = "PPI network", choices = ppi_networks)
+  })
+  observeEvent(input$load_input, {
+    module_object <- MODifieRDB::modulediscoverer(input_name = input$input_object, 
+                                          ppi_name = input$ppi_object, 
+                                          permutations = 100,
+                                          deg_cutoff = 0.05,
+                                          module_name = input$module_name,
+                                          repeats = 3)
+    
+    
+  })
+}
+    
+## To be copied in the UI
+# mod_Modulediscoverer_ui("Modulediscoverer_ui_1")
+    
+## To be copied in the server
+# callModule(mod_Modulediscoverer_server, "Modulediscoverer_ui_1")
+ 
