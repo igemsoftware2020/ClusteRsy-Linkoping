ce72223958454d460ac1006c950a2cac5a4a39de
Lucas Porcile <lucasporcile@Lucass-MacBook-Pro.local>
2020-06-17 12:53:35 +0200

    Finished DiffCoEx

--- R/mod_DiffCoEx.R
+++ R/mod_DiffCoEx.R
@@ -12,21 +12,43 @@ mod_DiffCoEx_ui <- function(id){
   tagList(
     uiOutput(ns("input_choice")),
     uiOutput(ns("ppi_choice")),
+   
     textInput(ns("module_name"), "Module object name"),
-    actionButton(ns("load_input"), "Infer DiffCoEx module"),
-    fluidPage(theme = shinytheme("flatly"),
-    radioButtons(clustermethod, "Select a cluster method:", 
+    selectInput(ns("cluster_method"), "Select an agglomeration method", 
                  c("ward",
                    "single",
                    "complete",
                    "average",
                    "mcquitty",
                    "median",
-                   "centroid",
-                   plotOutput("clusterPlot"))
+                   "centroid"),
+                selected = "ward",
+                multiple = FALSE,
+                selectize = TRUE,
+                width = NULL),
+    selectInput(ns("cor_method"), "Select an correlation coefficent",
+                c("pearson",
+                  "kendall",
+                  "spearman"),
+                selected = "pearson",
+                multiple = FALSE,
+                selectize = TRUE,
+                width = NULL),
+    selectInput(ns("cuttree_method"), "Select a method to use",
+                c("hybrid",
+                  "tree"),
+                multiple = FALSE,
+                selectize = TRUE,
+                width = NULL),
+   
+     htmlOutput(ns("method")),   
+    
+    sliderInput(ns("minClusterSize"), label = "minimum cluster size", min = 0, max = 100, value = 5),
+    sliderInput(ns("cut_height"), label = "maximum joining heights", min = 0, max = 1, value = 0.1),
+    sliderInput(ns("pval_cutoff"), label = "p-value cut-off", min = 0, max = 1, value = 0.05),
+    actionButton(ns("load_input"), "Infer DiffCoEx module")
     )
-    )
-  )
+  
 }
     
 #' DiffCoEx Server Function
@@ -35,6 +57,21 @@ mod_DiffCoEx_ui <- function(id){
 mod_DiffCoEx_server <- function(input, output, session){
   ns <- session$ns
  
+  observeEvent(input$cuttree_method, {
+               if (input$cuttree_method == "hybrid") {output$method <- 
+    renderUI({
+      tagList(
+      sliderInput(ns("deepSplit"), label = "sensitivity to cluster splitting", min = 1, max = 4, value = 0.1),
+      numericInput(ns("beta"), label = "soft thresholding power (%)", value = 99, max = 100, min = 0),
+      prettySwitch(ns("pamRespectsDendro"), label = "PAM respects dendrogram", value = FALSE, status = "warning"))})}
+    else {output$method <- 
+      renderUI({
+      tagList(
+      prettySwitch(ns("deepSplit"), label = "sensitivity to cluster splitting", value = FALSE, status = "warning"),
+      numericInput(ns("beta"), label = "soft thresholding power (%)", value = 0.99, max = 100, min = 0))})}
+    })
+    
+    
    output$input_choice <- renderUI({
     input_objects <- unlist(MODifieRDB::get_available_input_objects(con)$input_name)
     selectInput(ns("input_object"), label = "Input object", choices = input_objects)
@@ -44,27 +81,20 @@ mod_DiffCoEx_server <- function(input, output, session){
     ppi_networks <- unlist(MODifieRDB::get_available_networks(con))
     selectInput(ns("ppi_object"), label = "PPI network", choices = ppi_networks)
   })
-  
-  {
-    output$clusterPlot <- renderPlot({
-      dist <- switch(input$clustermethod,
-                     ward = ward,
-                     single = single,
-                     complete = complete,
-                     average = average,
-                     mcquitty = mcquitty,
-                     median = median,
-                     centroid = centroid
-                     )
-      
-      hist(clustermethod(500))
-    })
-  }
+
   observeEvent(input$load_input, {
-    module_object <- MODifieRDB::diffcoex(input_name = input$input_object, 
-                                          ppi_name = input$ppi_object, 
-                                          deg_cutoff = .98,
-                                          cluster_method = ,
+    module_object <- MODifieRDB::DiffCoEx(input_name = input$input_object, 
+                                          ppi_name = input$ppi_object,
+                                          cluster_method = input$cluster_method,
+                                          cor_method = input$cor_method,
+                                          cuttree_method = input$cuttree_method,
+                                          minClusterSize = input$minClusterSize,
+                                          deepSplit = input$deepSplit,
+                                          pamRespectsDendro = input$pamRespectsDendro,
+                                          cut_height = input$cut_height,
+                                          pval_cutoff = input$pval_cutoff,
+                                          beta = input$beta,
+                                          
                                           module_name = input$module_name,
                                           con = con)
     
