62ce07476f4fee740ea4443e7f4cbaa0f00ef11c
Lucas Porcile <lucasporcile@Lucass-MacBook-Pro.local>
2020-06-17 13:45:07 +0200

    Starrted CClique

--- R/mod_CClique.R
+++ R/mod_CClique.R
@@ -10,7 +10,18 @@
 mod_CClique_ui <- function(id){
   ns <- NS(id)
   tagList(
- 
+    uiOutput(ns("input_choice")),
+    uiOutput(ns("ppi_choice")),
+    
+    sliderInput(ns("frequency_cutoff"), label = "Select frequency cutoff", min = 0, max = 100, value = 50),
+    sliderInput(ns("fraction_of_interactions"), label = "Fraction of interactions", min = 0, max = 1, value = 0.4),
+    sliderInput(ns("deg_cutoff"), label = "P-value cutoff", min = 0, max = 1, value = 0.05),
+    prettySwitch(ns("to_db"), label = "Save result in database", value = TRUE, status = "warning"),
+    numericInput(ns("iteration"), label = "Number of iterations", value = 50, max = 100, min = 0),
+    numericInput(ns("n_cores"), label = "Number of cores", value = 3, max = 50, min = 0),
+    prettySwitch(ns("multiple_cores"), label = "Parallellize iterations", value = TRUE, status = "warning"),
+    actionButton(ns("load_input"), "Infer Correlation clique module"),
+    
   )
 }
     
@@ -20,6 +31,27 @@ mod_CClique_ui <- function(id){
 mod_CClique_server <- function(input, output, session){
   ns <- session$ns
  
+   output$input_choice <- renderUI({
+    input_objects <- unlist(MODifieRDB::get_available_input_objects(con)$input_name)
+    selectInput(ns("input_object"), label = "Input object", choices = input_objects)
+  })
+  
+  output$ppi_choice <- renderUI({
+    ppi_networks <- unlist(MODifieRDB::get_available_networks(con))
+    selectInput(ns("ppi_object"), label = "PPI network", choices = ppi_networks)
+  })
+  observeEvent(input$load_input, {
+    module_object <- MODifieRDB::diamond_db(input_name = input$input_object, 
+                                            ppi_name = input$ppi_object, 
+                                            frequency_cutoff = input$frequency_cutoff,
+                                            to_db = input$to_db,
+                                            fraction_of_interactions = input$fraction_of_interactions,
+                                            deg_cutoff = input$deg_cutoff,
+                                            multiple_cores = input$multiple_cores,
+                                            con = con)
+    
+    
+  })
 }
     
 ## To be copied in the UI
