36918ae97f8fcb317054ca35f4df4a422e5fe746
Lucas Porcile <lucasporcile@Lucass-MacBook-Pro.local>
2020-06-18 10:10:32 +0200

    Added parameters for cliquesum

--- R/mod_CliqueSum.R
+++ R/mod_CliqueSum.R
@@ -11,7 +11,15 @@ mod_CliqueSum_ui <- function(id){
   ns <- NS(id)
   tagList(
     uiOutput(ns("input_choice")),
+    uiOutput(ns("ppi_choice")),
+    textInput(ns("module_name"), "Module object name"),
     sliderInput(ns("deg_cutoff"), label = "P-value cutoff for DEGs", min = 0, max = 1, value = 0.05),
+    sliderInput(ns("clique_significance"), label = "Clique significance", min = 0, max = 1, value = 0.05),
+    numericInput(ns("n_cores"), label = "Number of cores", value = 3, max = 50, min = 0),
+    numericInput(ns("min_clique_size"), label = "Minimal clique size", value = 2, max = 50, min = 2),
+    prettySwitch(ns("multiple_cores"), label = "Parallellize iterations", value = TRUE, status = "warning"),
+    numericInput(ns("n_iterations"), label = "Iterations", value = , max = 10000, min = 0),
+    prettySwitch(ns("to_db"), label = "Save result in database", value = TRUE, status = "warning"),
   )
 }
     
@@ -26,6 +34,20 @@ mod_CliqueSum_server <- function(input, output, session){
     selectInput(ns("input_object"), label = "Input object", choices = input_objects)
   })
   
+  observeEvent(input$load_input, {
+    module_object <- MODifieRDB::DiffCoEx(input_name = input$input_object,
+                                          ppi_name = input$ppi_object,
+                                          n_iterations = input$n_iterations,
+                                          deg_cutoff = input$deg_cutoff,
+                                          clique_significance = input$clique_significance,
+                                          min_clique_size = input$min_clique_size,
+                                          multiple_cores = input$multiple_cores,
+                                          n_cores = input$n_cores,
+                                          module_name = input$module_name,
+                                          to_db = input$to_db,
+                                          con = con)
+                                          
+  })
 }
     
 ## To be copied in the UI
