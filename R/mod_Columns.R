56f1d43ea2ba15ffc292587fc6670fcb0bfb89ec
AdamLaang <65313177+AdamLaang@users.noreply.github.com>
2020-06-18 20:48:47 +0200

    Started to implement select_input for the main page

--- R/mod_Columns.R
+++ R/mod_Columns.R
@@ -18,14 +18,18 @@ mod_Columns_ui <- function(id){
                                         tags$span(
                                           class="label", "1",
                                           style = "border-radius: 100%;background-color:#ffbd40")))),
-             htmlOutput(ns("algorithm"))),
+             htmlOutput(ns("algorithm")),
+             ),
     
     # Module containers          
     tags$div(`class`="row",
              tags$div(`class`="col-sm-4",
                       tags$form(class = "well",
                                 `style`="background-color:#2c3e50;",
-                                mod_upload_ui(ns("upload_ui_1")))),
+                                uiOutput(ns("input_choice")),
+                                mod_upload_ui(ns("upload_ui_1"))
+                                ),
+                      ),
              htmlOutput(ns("algorithm1"))))
 }
     
@@ -34,20 +38,27 @@ mod_Columns_ui <- function(id){
 #' @noRd 
 mod_Columns_server <- function(input, output, session, con){
   ns <- session$ns
+  
   upload_ui_1 <- callModule(mod_upload_server, "upload_ui_1", con = con)
   observeEvent(upload_ui_1$module, {
     MODifieR_module <- upload_ui_1$module
   }
   )
   
+  output$input_choice <- renderUI({
+    input_objects <- unlist(MODifieRDB::get_available_input_objects(con)$input_name)
+    selectInput(ns("input_object"), label = "Input object", choices = input_objects, selected = NULL) #Problem with selected input that triggers the second column.
+  })
+ 
   upload_algorithm <- reactive({
-    req((upload_ui_1$module))
+    req(upload_ui_1$module | input$input_object) #Can't trigger the OR operator due to input_object not logical or numerical
+   
     if (is.null(upload_ui_1$module)){
       
-      return(NULL)
+       return(NULL)
     }
   })
-  
+
   # Number conainters
   output$algorithm <- renderUI({
     upload_algorithm()
