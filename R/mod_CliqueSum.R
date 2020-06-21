6da40f8f45a53fed9f32aac35949e5cc2301d29a
AdamLaang <65313177+AdamLaang@users.noreply.github.com>
2020-06-21 18:02:27 +0200

    Fixed an error + code cleaning

--- R/mod_CliqueSum.R
+++ R/mod_CliqueSum.R
@@ -27,7 +27,6 @@ if (nrow(MODifieRDB::get_available_db_networks(con)) != 0 ) {
     tagList(
       uiOutput(ns("input_choice")),
       textInput(ns("module_name"), "Module object name"),
-      sliderInput(ns("deg_cutoff"), label = "P-value cutoff for DEGs", min = 0, max = 1, value = 0.05),
       sliderInput(ns("clique_significance"), label = "Clique significance", min = 0, max = 1, value = 0.05),
       numericInput(ns("n_cores"), label = "Number of cores", value = 3, max = 50, min = 0),
       numericInput(ns("min_clique_size"), label = "Minimal clique size", value = 2, max = 50, min = 2),
@@ -56,7 +55,7 @@ if (nrow(MODifieRDB::get_available_db_networks(con)) != 0 ) {
     })
     
     observeEvent(input$build_db, {
-      id <- showNotification("Creating clique database", duration = NULL, closeButton = FALSE)
+      id <- showNotification("Creating clique database", duration = NULL, closeButton = FALSE, type = "warning")
       
       clique_db <- MODifieRDB::build_clique_db_db(ppi_name = input$ppi_object,
                                                   db_folder =  "." , #This should be connected to the db.
@@ -75,7 +74,6 @@ if (nrow(MODifieRDB::get_available_db_networks(con)) != 0 ) {
       tagList(
         uiOutput(ns("input_choice")),
         textInput(ns("module_name"), "Module object name"),
-        sliderInput(ns("deg_cutoff"), label = "P-value cutoff for DEGs", min = 0, max = 1, value = 0.05),
         sliderInput(ns("clique_significance"), label = "Clique significance", min = 0, max = 1, value = 0.05),
         numericInput(ns("n_cores"), label = "Number of cores", value = 3, max = 50, min = 0),
         numericInput(ns("min_clique_size"), label = "Minimal clique size", value = 2, max = 50, min = 2),
@@ -95,17 +93,17 @@ if (nrow(MODifieRDB::get_available_db_networks(con)) != 0 ) {
   
   observeEvent(input$load_input, {
     module_object <- MODifieRDB::clique_sum_db(input_name = input$input_object,
-                                          ppi_name = input$ppi_object,
-                                          n_iterations = input$n_iterations,
-                                          deg_cutoff = input$deg_cutoff,
-                                          clique_significance = input$clique_significance,
-                                          min_clique_size = input$min_clique_size,
-                                          multiple_cores = input$multiple_cores,
-                                          n_cores = input$n_cores,
-                                          module_name = input$module_name,
-                                          to_db = input$to_db,
-                                          con = con)
-                                          
+                                                ppi_name = input$ppi_object,
+                                                n_iterations = input$n_iterations,
+                                                to_db = input$to_db,
+                                                folder = FALSE, #Should be changed later on if the user wants to save results locally.
+                                                clique_significance = input$clique_significance,
+                                                min_clique_size = input$min_clique_size,
+                                                multiple_cores = input$multiple_cores,
+                                                n_cores = input$n_cores,
+                                                module_name = input$module_name,
+                                                con = con)
+    
   })
 }
     
