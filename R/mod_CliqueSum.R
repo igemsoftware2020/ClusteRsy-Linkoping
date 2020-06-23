64a6c195414f25d6d7034de6cbb7156c531cd11a
Hyperty <56047053+Hypertyz@users.noreply.github.com>
2020-06-23 20:37:28 +0200

    FIX BUGS, CLEAN CODE AND UPDATE UI - fix some bugs in the CliqueSum module

--- R/mod_CliqueSum.R
+++ R/mod_CliqueSum.R
@@ -20,32 +20,26 @@ mod_CliqueSum_ui <- function(id){
 #' @noRd 
 mod_CliqueSum_server <- function(input, output, session, con){
   ns <- session$ns
-  
-  
-  output$ppi_choice <- renderUI({
-    ppi_networks <- unlist(MODifieRDB::get_available_networks(con))
-    
-    selectInput(ns("ppi_object"), label = "PPI network", choices = ppi_networks, popup = "Protein-Protein interaction network to overlay the differentially expressed genes on")
-  })
-  
-  
+
+  UI <- tagList(
+    uiOutput(ns("input_choice")),
+    uiOutput(ns("ppi_choice")),
+    tags$div(id = "error_name_CliqueSum_js",
+             textInput(ns("module_name"), "Module object name", popup = "Object that is produced by the disease module inference methods")),
+    uiOutput(ns("error_name_descrip")),
+    uiOutput(ns("error_name_js")),
+    sliderInput(ns("clique_significance"), label = "Clique significance", min = 0, max = 1, value = 0.05, popup ="P-value for cliques to be considered significant"),
+    numericInput(ns("min_clique_size"), label = "Minimal clique size", value = 2, max = 50, min = 2, popup = "Minimal size of cliques"),
+    numericInput(ns("n_iterations"), label = "Iterations", value = 500, max = 10000, min = 0, popup = "Number of iterations to be performed for the permutation based P-value"),
+    tags$div(style = "text-align:center",
+             actionButton(ns("load_input"), label = "Infer Clique Sum module")
+    )
+  )
   
   if (nrow(MODifieRDB::get_available_db_networks(con)) != 0 ) {
     output$parameters <- renderUI({
-      tagList(
-        uiOutput(ns("input_choice")),
-        uiOutput(ns("ppi_choice")),
-        tags$div(id = "error_name_DiffCoEx_js",
-                 textInput(ns("module_name"), "Module object name", popup = "Object that is produced by the disease module inference methods")),
-        uiOutput(ns("error_name_descrip")),
-        uiOutput(ns("error_name_js")),
-        sliderInput(ns("clique_significance"), label = "Clique significance", min = 0, max = 1, value = 0.05, popup ="P-value for cliques to be considered significant"),
-        numericInput(ns("min_clique_size"), label = "Minimal clique size", value = 2, max = 50, min = 2, popup = "Minimal size of cliques"),
-        numericInput(ns("n_iterations"), label = "Iterations", value = 500, max = 10000, min = 0, popup = "Number of iterations to be performed for the permutation based P-value"),
-        tags$div(style = "text-align:center",
-                 actionButton(ns("load_input"), label = "Infer Clique Sum module")
-        )
-      )
+      UI
+
     })
   } else {
     
@@ -58,9 +52,10 @@ mod_CliqueSum_server <- function(input, output, session, con){
       )
     })
     
-    
-    
+
     observeEvent(input$build_db, {
+      output$build_clique <- renderUI({})
+
       id <- showNotification("Creating clique database", duration = NULL, closeButton = FALSE, type = "warning")
       
       clique_db <- MODifieRDB::build_clique_db_db(ppi_name = input$ppi_object,
@@ -69,31 +64,12 @@ mod_CliqueSum_server <- function(input, output, session, con){
                                                   con = con
       )
       on.exit(removeNotification(id), add = TRUE)
-      
-    })
     
-  }
-  
-  observeEvent(input$build_db, {
-    output$build_clique <- NULL
-    output$parameters <- renderUI({
-      tagList(
-        uiOutput(ns("input_choice")),
-        uiOutput(ns("ppi_choice")),
-        tags$div(id = "error_name_DiffCoEx_js",
-                 textInput(ns("module_name"), "Module object name", popup = "Object that is produced by the disease module inference methods")),
-        uiOutput(ns("error_name_descrip")),
-        uiOutput(ns("error_name_js")),
-        sliderInput(ns("clique_significance"), label = "Clique significance", min = 0, max = 1, value = 0.05, popup ="P-value for cliques to be considered significant"),
-        numericInput(ns("min_clique_size"), label = "Minimal clique size", value = 2, max = 50, min = 2, popup = "Minimal size of cliques"),
-        numericInput(ns("n_iterations"), label = "Iterations", value = 500, max = 10000, min = 0, popup = "Number of iterations to be performed for the permutation based P-value"),
-        tags$div(style = "text-align:center",
-                 actionButton(ns("load_input"), label = "Infer Clique Sum module")
-        )
-      )
+      output$parameters <- renderUI({
+        UI
+      })
     })
   }
-  )
   
   module_name <- reactive({
     input$module_name
@@ -102,21 +78,30 @@ mod_CliqueSum_server <- function(input, output, session, con){
   observe({
     if (any(MODifieRDB::get_available_module_objects(con)$module_name == module_name())){
       output$error_name_js <- renderUI({
-        tags$script(HTML("element = document.getElementById('error_name_DiffCoEx_js');
+
+        tags$script(HTML("element = document.getElementById('error_name_CliqueSum_js');
                        element.classList.add('has-error');
-                       document.getElementById('main_page_v2_ui_1-Columns_ui_1-Description1_ui_1-DiffCoEx_ui_1-load_input').disabled = true;"))
+                       document.getElementById('main_page_v2_ui_1-Columns_ui_1-Description1_ui_1-CliqueSum_ui_1-load_input').disabled = true;"))
       })
       output$error_name_descrip <- renderUI({
         tags$p(class = "text-danger", tags$b("Error:"), "This name has been taken. Please try again!")
       })
     } else {
       output$error_name_js <- renderUI({
-        tags$script(HTML("document.getElementById('error_name_DiffCoEx_js').classList.remove('has-error');
-                         document.getElementById('main_page_v2_ui_1-Columns_ui_1-Description1_ui_1-DiffCoEx_ui_1-load_input').disabled = false;"))
+        tags$script(HTML("document.getElementById('error_name_CliqueSum_js').classList.remove('has-error');
+                         document.getElementById('main_page_v2_ui_1-Columns_ui_1-Description1_ui_1-CliqueSum_ui_1-load_input').disabled = false;"))
+
       })
       output$error_name_descrip <- NULL
     }
   }) 
+  
+
+  output$ppi_choice <- renderUI({
+    ppi_networks <- unlist(MODifieRDB::get_available_networks(con))
+    
+    selectInput(ns("ppi_object"), label = "PPI network", choices = ppi_networks, popup = "Protein-Protein interaction network to overlay the differentially expressed genes on")
+  })
   
   output$input_choice <- renderUI({
     input_objects <- unlist(MODifieRDB::get_available_input_objects(con)$input_name)
@@ -126,8 +111,9 @@ mod_CliqueSum_server <- function(input, output, session, con){
   
   observeEvent(input$load_input, {
     id <- showNotification("Infering method", duration = NULL, closeButton = FALSE, type = "warning")
-    output$error_p_value <- NULL # I CANNOT REMOVE THIS BUG, SO THIS IS A FEATURE NOW :)
-    module_object <- try(MODifieRDB::clique_sum_db(input_name = input$input_object,
+
+    module_object <- MODifieRDB::clique_sum_db(input_name = input$input_object,
+
                                                ppi_name = input$ppi_object,
                                                n_iterations = input$n_iterations,
                                                clique_significance = input$clique_significance,
@@ -136,15 +122,10 @@ mod_CliqueSum_server <- function(input, output, session, con){
                                                n_cores = 1,
                                                module_name = input$module_name,
                                                con = con)
-    )
     
     on.exit(removeNotification(id), add = TRUE)
     
-    if (class(module_object) == "try-error"){
-      output$error_p_value <- renderUI({
-        tags$p(class = "text-danger", tags$b("Error:"), module_object)
-      })
-    }
+
   })
   
   
