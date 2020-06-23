4c170d43f0a1215eb1d3509fdb6eb5489ff34453
AdamLaang <65313177+AdamLaang@users.noreply.github.com>
2020-06-23 12:01:53 +0200

    Fixer error and code cleaning

--- R/mod_CliqueSum.R
+++ R/mod_CliqueSum.R
@@ -10,36 +10,40 @@
 mod_CliqueSum_ui <- function(id){
   ns <- NS(id)
   tagList(
-        uiOutput(ns("parameters")),
-        uiOutput(ns("build_clique"))
+    uiOutput(ns("parameters")),
+    uiOutput(ns("build_clique"))
   )
 }
-    
+
 #' CliqueSum Server Function
 #'
 #' @noRd 
 mod_CliqueSum_server <- function(input, output, session, con){
   ns <- session$ns
   
-if (nrow(MODifieRDB::get_available_db_networks(con)) != 0 ) {
-  
-  output$parameters <- renderUI({
-    tagList(
-      uiOutput(ns("input_choice")),
-      textInput(ns("module_name"), "Module object name"),
-      sliderInput(ns("clique_significance"), label = "Clique significance", min = 0, max = 1, value = 0.05),
-      numericInput(ns("n_cores"), label = "Number of cores", value = 3, max = 50, min = 0),
-      numericInput(ns("min_clique_size"), label = "Minimal clique size", value = 2, max = 50, min = 2),
-      shinyWidgets::prettySwitch(ns("multiple_cores"), label = "Multiple cores", value = TRUE, status = "warning"),
-      numericInput(ns("n_iterations"), label = "Iterations", value = 500, max = 10000, min = 0),
-      shinyWidgets::prettySwitch(ns("to_db"), label = "Save result in database", value = TRUE, status = "warning"),
-      tags$div(style = "text-align:center",
-      actionButton(ns("load_input"), label = "Infer Clique Sum module")
-      )
-    )
+  output$ppi_choice <- renderUI({
+    ppi_networks <- unlist(MODifieRDB::get_available_networks(con))
+    
+    selectInput(ns("ppi_object"), label = "PPI network", choices = ppi_networks)
   })
-} else {
-
+  
+  if (nrow(MODifieRDB::get_available_db_networks(con)) != 0 ) {
+    
+    output$parameters <- renderUI({
+      tagList(
+        uiOutput(ns("input_choice")),
+        uiOutput(ns("ppi_choice")),
+        textInput(ns("module_name"), "Module object name"),
+        sliderInput(ns("clique_significance"), label = "Clique significance", min = 0, max = 1, value = 0.05),
+        numericInput(ns("min_clique_size"), label = "Minimal clique size", value = 2, max = 50, min = 2),
+        numericInput(ns("n_iterations"), label = "Iterations", value = 500, max = 10000, min = 0),
+        tags$div(style = "text-align:center",
+                 actionButton(ns("load_input"), label = "Infer Clique Sum module")
+        )
+      )
+    })
+  } else {
+    
     output$build_clique <- renderUI({
       tagList(
         uiOutput(ns("ppi_choice")),
@@ -48,11 +52,7 @@ if (nrow(MODifieRDB::get_available_db_networks(con)) != 0 ) {
       )
     })
     
-    output$ppi_choice <- renderUI({
-      ppi_networks <- unlist(MODifieRDB::get_available_networks(con))
-      
-      selectInput(ns("ppi_object"), label = "PPI network", choices = ppi_networks)
-    })
+    
     
     observeEvent(input$build_db, {
       id <- showNotification("Creating clique database", duration = NULL, closeButton = FALSE, type = "warning")
@@ -66,20 +66,18 @@ if (nrow(MODifieRDB::get_available_db_networks(con)) != 0 ) {
       
     })
     
-}
-
+  }
+  
   observeEvent(input$build_db, {
     output$build_clique <- NULL
     output$parameters <- renderUI({
       tagList(
         uiOutput(ns("input_choice")),
+        uiOutput(ns("ppi_choice")),
         textInput(ns("module_name"), "Module object name"),
-        sliderInput(ns("clique_significance"), label = "Clique significance", min = 0, max = 1, value = 0.05),
-        numericInput(ns("n_cores"), label = "Number of cores", value = 3, max = 50, min = 0),
+        sliderInput(ns("clique_significance"), label = "Clique significance", min = 0, max = 1, value = 0.01),
         numericInput(ns("min_clique_size"), label = "Minimal clique size", value = 2, max = 50, min = 2),
-        shinyWidgets::prettySwitch(ns("multiple_cores"), label = "Multiple cores", value = TRUE, status = "warning"),
         numericInput(ns("n_iterations"), label = "Iterations", value = 500, max = 10000, min = 0),
-        shinyWidgets::prettySwitch(ns("to_db"), label = "Save result in database", value = TRUE, status = "warning"),
         actionButton(ns("load_input"), label = "Infer Clique Sum module")
       )
     })
@@ -92,27 +90,28 @@ if (nrow(MODifieRDB::get_available_db_networks(con)) != 0 ) {
   })
   
   observeEvent(input$load_input, {
-    id <- showNotification("Creating input object", duration = NULL, closeButton = FALSE, type = "warning")
-    on.exit(removeNotification(id), add = TRUE)
+    id <- showNotification("Infering method", duration = NULL, closeButton = FALSE, type = "warning")
+    
     module_object <- MODifieRDB::clique_sum_db(input_name = input$input_object,
-                                                ppi_name = input$ppi_object,
-                                                n_iterations = input$n_iterations,
-                                                to_db = input$to_db,
-                                                folder = FALSE, #Should be changed later on if the user wants to save results locally.
-                                                clique_significance = input$clique_significance,
-                                                min_clique_size = input$min_clique_size,
-                                                multiple_cores = input$multiple_cores,
-                                                n_cores = input$n_cores,
-                                                module_name = input$module_name,
-                                                con = con)
+                                               ppi_name = input$ppi_object,
+                                               n_iterations = input$n_iterations,
+                                               clique_significance = input$clique_significance,
+                                               min_clique_size = input$min_clique_size,
+                                               multiple_cores = FALSE,
+                                               n_cores = 1,
+                                               module_name = input$module_name,
+                                               con = con)
+    
+    on.exit(removeNotification(id), add = TRUE)
     
   })
-
+  
+  
 }
-    
+
 ## To be copied in the UI
 # mod_CliqueSum_ui("CliqueSum_ui_1")
-    
+
 ## To be copied in the server
 # callModule(mod_CliqueSum_server, "CliqueSum_ui_1")
- 
+
