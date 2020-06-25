64e7e793fa3ad16f8b52be6e1776af18a5cc8e02
Lucas Porcile <lucasporcile@Lucass-MacBook-Pro.local>
2020-06-25 13:27:47 +0200

    fixed stuff

--- R/mod_DOSE.R
+++ R/mod_DOSE.R
@@ -16,7 +16,7 @@ mod_DOSE_ui <- function(id){
                  choices = c("Disease Ontology (DO)",
                              "Network of Cancer Gene (NCG)",
                              "DisGeNET (DGN)"),
-                          
+                 htmlOutput(ns("para"))
  
   ))
   
@@ -25,7 +25,7 @@ mod_DOSE_ui <- function(id){
 #' DOSE Server Function
 #'
 #' @noRd 
-mod_DOSE_server <- function(input, output, session){
+mod_DOSE_server <- function(input, output, session, con){
   ns <- session$ns
   output$module_input <- renderUI({
     module_objects <- unlist(MODifieRDB::get_available_module_objects(con)$module_name)
@@ -33,14 +33,18 @@ mod_DOSE_server <- function(input, output, session){
   })
   
   output$background_genes <- renderUI({
-    print(input$module_object)mo
+    print(input$module_object)
     ppi_networks <- unlist(MODifieRDB::get_available_networks(con))
     selectInput(ns("ppi_object"), label = "Background genes", choices = ppi_networks, popup = "The background genes are all the genes present in the PPI network")
   })
   
-  observeEvent(
+  enrich <- reactive({
+    input$enrich
+  })
+  
+  observeEvent(input$enrich, {
    if (input$enrich == "Disease Ontology (DO)") {
-       renderUI({
+     output$para <-renderUI({
          tagList(
            sliderInput(ns("pvalueCutoff"), label = "P-value cut-off", min = 0, max = 1, value = 0.05),
            selectInput(ns("pAdjustMethod"), "Select an adjustment method",
@@ -57,19 +61,19 @@ mod_DOSE_server <- function(input, output, session){
            numericInput(ns("minGSSize"), label = "Minimum size of genes", value = 5, max = 100, min = 0),
            numericInput(ns("minGSSize"), label = "Maximal size each geneSet", value = 500, max = 5000, min = 0),
            sliderInput(ns("qvalueCutoff"), label = "Q-value cut-off", min = 0, max = 1, value = 0.05),
-           helpText("Select parameters for gene set enrichment analysis"),
+           helpText(ns("Select parameters for gene set enrichment analysis")),
            sliderInput(ns("nperm"), label = "Permutation number", min = 0, max = 1000, value = 50),
            selectInput(ns("by"), label = "Select algorithm",
                        choices = c("fgsea",
                                    "DOSE")),
            actionButton(ns("load_inputDO"), label = "Find disease assosciation and create enrichment analysis object")
-   )})})
+   )})}})
    
   observeEvent(input$load_inputDO, {
-    id <- showNotification("Identifying disease assosciation", duration = NULL, closeButton = FALSE, type = "warning")
+    id <- showNotification("Identifying disease assosciation and creating enrichment analysis object", duration = NULL, closeButton = FALSE, type = "warning")
     on.exit(removeNotification(id), add = TRUE)
     module_genes <- MODifieRDB::MODifieR_module_from_db(input$module_object, con = con)$module_genes
-    enrichment_objectONE <- try(clusterProfiler::enrichDO(gene = module_genes,
+    enrichment_objectONE <- try(DOSE::enrichDO(gene = module_genes,
                                                          ont = "DO",
                                                          pvalueCutoff = input$deg_cutoff,
                                                          pAdjustMethod = input$padj_method,
@@ -81,46 +85,45 @@ mod_DOSE_server <- function(input, output, session){
                                                          
     )
     )
-    if (class(enrichment_object) == "try-error"){
+    if (class(enrichment_objectONE) == "try-error"){
       output$error_p_value <- renderUI({
-        tags$p(class = "text-danger", tags$b("Error:"), enrichment_object)
+        tags$p(class = "text-danger", tags$b("Error:"), enrichment_objectONE)
       })
     }
-    id <- showNotification("Creating enrichment analysis object", duration = NULL, closeButton = FALSE, type = "warning")
-    on.exit(removeNotification(id), add = TRUE)
     module_genes <- MODifieRDB::MODifieR_module_from_db(input$module_object, con = con)$module_genes
-    enrichment_objectTWO <- try(clusterProfiler::gseDO(gene = module_genes,
-                                                       ont = "DO",
+    enrichment_objectTWO <- try(DOSE::gseDO(geneList = module_genes,
+                                                       ont ="DO",
+                                                       exponent = 1,
                                                        nperm = input$nperm,
                                                        pvalueCutoff = input$deg_cutoff,
                                                        pAdjustMethod = input$padj_method,
-                                                       universe = input$ppi_object,
                                                        minGSSize = input$mingssize,
                                                        maxGSSize = input$maxgssize,
                                                        qvalueCutoff = input$qvalue_cutoff,
                                                        by = input$by,
+                                                       seed = FALSE,
                                                        verbose = FALSE,
                                                        
     )
     )
-    if (class(enrichment_object) == "try-error"){
+    if (class(enrichment_objectTWO) == "try-error"){
       output$error_p_value <- renderUI({
-        tags$p(class = "text-danger", tags$b("Error:"), enrichment_object)
+        tags$p(class = "text-danger", tags$b("Error:"), enrichment_objectTWO)
       })
     }
   })
   #these two are the remaining ones
-   observeEvent(
+   observeEvent(input$enrich, {
      if (input$enrich == "Network of Cancer Gene (NCG)") {
        renderUI({
          tagList()
-       })})
+       })}})
      
-     observeEvent(
+     observeEvent(input$enrich, {
        if (input$enrich == "DisGeNET (DGN)") {
          renderUI({
            tagList()
-         })})
+         })}})
  
      
 }
--- R/mod_visual.R
+++ R/mod_visual.R
@@ -10,6 +10,7 @@
 mod_visual_ui <- function(id){
   ns <- NS(id)
   tagList(
+    mod_DOSE_ui("DOSE_ui_1"),
     tags$div(`class`="jumbotron",
              tags$h1("Hello,world!",class="display-3"),
              tags$hr(class="my-4"),
@@ -28,6 +29,7 @@ mod_visual_ui <- function(id){
 #' @noRd 
 mod_visual_server <- function(input, output, session, con){
   ns <- session$ns
+  callModule(mod_DOSE_server, "DOSE_ui_1")
 
 }
 
