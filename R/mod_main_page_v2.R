39b764072ac18ea6e5d866df301fbfc1b86415e2
Hyperty <56047053+Hypertyz@users.noreply.github.com>
2020-06-26 11:33:34 +0200

    CHANGE THE HOME BUTTON

--- R/mod_main_page_v2.R
+++ R/mod_main_page_v2.R
@@ -10,14 +10,18 @@
 mod_main_page_v2_ui <- function(id){
   ns <- NS(id)
   tagList(
-    navbarPage(title =  "MODifieRWeb", collapsible = TRUE,
-               tabPanel(title = tags$i(class="fa fa-home"), mod_welcoming_page_ui(ns("welcoming_page_ui_1"))),
+    navbarPage(id = ns("navbar"), title = actionLink(ns("title"), tags$i(class = "fa fa-home", `aria-hidden`="true"), 
+                                                     type = "heading", style="left: 9px; position: relative;"), 
+               collapsible = TRUE,
                tabPanel("Input data", mod_Columns_ui(ns("Columns_ui_1"))),
                tabPanel("Visualization", mod_visual_ui(ns("visual_ui_1"))),
                tabPanel("Input objects", mod_input_overview_ui(ns("input_overview_ui_1"))),
                tabPanel("Module objects", mod_module_overview_ui(ns("module_overview_ui_1"))),
-               tabPanel("PPI networks", mod_ppi_networks_ui(ns("ppi_networks_ui_1")))
-    ))
+               tabPanel("PPI networks", mod_ppi_networks_ui(ns("ppi_networks_ui_1"))),
+               tabPanel(" ", mod_welcoming_page_ui(ns("welcoming_page_ui_1"))) #Hide this with Javascirpt
+    ),
+    tags$script(HTML("element = document.getElementsByTagName('a')[6]; element.style.display = 'none'")) #Change the number according to the tabPanel order
+  )
 }
 
 #' main_page_v2 Server Function
@@ -26,6 +30,9 @@ mod_main_page_v2_ui <- function(id){
 mod_main_page_v2_server <- function(input, output, session){
   ns <- session$ns
   con <- MODifieRDB::connect_to_db("./../testdb.db")
+  observeEvent(input$title, {
+    updateNavbarPage(session, "navbar", " ")
+  })
   callModule(mod_welcoming_page_server, "welcoming_page_ui_1")
   callModule(mod_visual_server, "visual_ui_1", con = con)
   callModule(mod_Columns_server, "Columns_ui_1", con = con)
