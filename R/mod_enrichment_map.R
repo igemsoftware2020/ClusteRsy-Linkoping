5de0c7579bcb06aafab9b5419f0862c91b4bfb7e
AdamLaang <adamlanglang@gmail.com>
2020-07-06 23:23:37 +0200

    Created enrichment map and made it reactive

---
+++ R/mod_enrichment_map.R
@@ -0,0 +1,41 @@
+#' enrichment_map UI Function
+#'
+#' @description A shiny Module.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd 
+#'
+#' @importFrom shiny NS tagList 
+mod_enrichment_map_ui <- function(id){
+  ns <- NS(id)
+  tagList(
+ plotlyOutput(ns("enrichment_plot")),
+ # selectInput(ns("color"), 
+ #             choices = c())
+  )
+}
+    
+#' enrichment_map Server Function
+#'
+#' @noRd 
+mod_enrichment_map_server <- function(input, output, session, selected, con){
+  ns <- session$ns
+  
+  
+  
+  output$enrichment_plot <- renderPlotly({
+    enrichment_object <- MODifieRDB::enrichment_object_from_db(con,
+                                                               selected$selected_object
+    )
+    
+    ggplotly(enrichplot::emapplot(enrichment_object, showCategory = 30, color = "p.adjust",  layout = "kk"))
+  })
+}
+    
+## To be copied in the UI
+# mod_enrichment_map_ui("enrichment_map_ui_1")
+    
+## To be copied in the server
+# callModule(mod_enrichment_map_server, "enrichment_map_ui_1")
+ 
