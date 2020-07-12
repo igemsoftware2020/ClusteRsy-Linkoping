05fc92665c1dafabb4c6cec974904ebe42f4a89f
AdamLaang <adamlanglang@gmail.com>
2020-07-12 10:40:06 +0200

    Added parameters to the enrichment map

--- R/mod_enrichment_map.R
+++ R/mod_enrichment_map.R
@@ -10,27 +10,35 @@
 mod_enrichment_map_ui <- function(id){
   ns <- NS(id)
   tagList(
- plotOutput(ns("enrichment_plot")),
- # selectInput(ns("color"), 
- #             choices = c())
+    plotOutput(ns("enrichment_map"))
   )
 }
     
 #' enrichment_map Server Function
 #'
 #' @noRd 
-mod_enrichment_map_server <- function(input, output, session, selected, con){
+mod_enrichment_map_server <- function(input, output, session, enrichment_map_para_ui_1, selected, con){
   ns <- session$ns
   
   
-  
-  output$enrichment_plot <- renderPlot({
-    enrichment_object <- MODifieRDB::enrichment_object_from_db(selected$selected_object,
-                                                               con
-    )
+  enrichment_map <- reactive({
     
-    enrichplot::emapplot(enrichment_object, showCategory = 30, color = "pvalue",  layout = "kk")
+    enrichment_object <- MODifieRDB::enrichment_object_from_db(selected$selected_object, con)
+    
+    p <- enrichplot::emapplot(enrichment_object,
+                                  showCategory = enrichment_map_para_ui_1$showcategory,
+                                  color = enrichment_map_para_ui_1$color,
+                                  layout = enrichment_map_para_ui_1$layout,
+                                  pie_scale = enrichment_map_para_ui_1$pie_scale,
+                                  line_scale = enrichment_map_para_ui_1$line_scale,
+                                  title = enrichment_map_para_ui_1$plot_title)
+    return(p)
   })
+  
+  output$enrichment_map <- renderPlot({
+    enrichment_map() #calling the reactive plot
+  })
+  
 }
     
 ## To be copied in the UI
---
+++ R/mod_enrichment_map_para.R
@@ -0,0 +1,66 @@
+#' enrichment_map_para UI Function
+#'
+#' @description A shiny Module.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd 
+#'
+#' @importFrom shiny NS tagList 
+mod_enrichment_map_para_ui <- function(id){
+  ns <- NS(id)
+  tagList(
+    #Parameters
+    tags$h3(class = "text-center",
+            "Select parameters"),
+   #Enrichment_map 
+    selectInput(ns("color"),
+                label = "Color",
+                choices = c("pvalue", "p.adjust", "qvalue")),
+    sliderInput(ns("showcategory"), 
+                label = "Number of enriched terms to display",
+                min = 5,
+                max = 50,
+                value = 10),
+    selectInput(ns("layout"), 
+                choices = c("kk",
+                            "nicely")),
+    sliderInput(ns("pie_scalue"), 
+                label = "Scale of pie plot", 
+                min = 0.1,
+                max = 5,
+                value = 1),
+    sliderInput(ns("line_scale"),
+                label = "Scale of line width",
+                min = 0.1,
+                max = 5,
+                value =1),
+    textInput(ns("title"), 
+              label = "Title")
+  )
+}
+    
+#' enrichment_map_para Server Function
+#'
+#' @noRd 
+mod_enrichment_map_para_server <- function(input, output, session){
+  ns <- session$ns
+  enrichment_map_para_module <- reactiveValues()
+  observe({
+    enrichment_map_module$color <- input$color
+    enrichment_map_module$showcategory <- input$showcategory
+    enrichment_map_module$layout <- input$layout
+    enrichment_map_module$pie_scale <- input$pie_scale
+    enrichment_map_module$line_scale <- input$line_scale
+    enrichment_map_module$title <- input$title
+  })
+  
+  return(enrichment_map_para_module)
+}
+    
+## To be copied in the UI
+# mod_enrichment_map_para_ui("enrichment_map_para_ui_1")
+    
+## To be copied in the server
+# callModule(mod_enrichment_map_para_server, "enrichment_map_para_ui_1")
+ 
