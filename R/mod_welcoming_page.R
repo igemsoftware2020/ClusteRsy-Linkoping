a984a1835b16ed433d270a5f5ac0e5586572f04d
AdamLaang <adamlanglang@gmail.com>
2020-07-20 19:27:32 +0200

    Added new front page button

--- R/mod_welcoming_page.R
+++ R/mod_welcoming_page.R
@@ -8,29 +8,6 @@
 #'
 #' @importFrom shiny NS tagList 
 #' 
-# button <- '<div  class="buttons">
-#   <button id= "blob_button" class="blob-btn">
-#     VIEW APP
-#     <span class="blob-btn__inner">
-#       <span class="blob-btn__blobs">
-#         <span class="blob-btn__blob"></span>
-#         <span class="blob-btn__blob"></span>
-#         <span class="blob-btn__blob"></span>
-#         <span class="blob-btn__blob"></span>
-#       </span>
-#     </span>
-#   </button>
-#   <br/>
-# <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
-#   <defs>
-#     <filter id="goo">
-#       <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10"></feGaussianBlur>
-#       <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 21 -7" result="goo"></feColorMatrix>
-#       <feBlend in2="goo" in="SourceGraphic" result="mix"></feBlend>
-#     </filter>
-#   </defs>
-# </svg>
-# </div>' # !!NOTE: The button only works on browser
 
 button <- '<button id = "blob_button" class ="front_button">
 View app </button>
