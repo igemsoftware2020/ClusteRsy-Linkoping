2072f1c37d574c0ed888bdb12d97889e24e5fd5b
AdamLaang <adamlanglang@gmail.com>
2020-08-09 12:00:49 +0200

    Added dependency packages

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -17,6 +17,9 @@
 ## Add one line by package you want to add as dependency
 usethis::use_package( "thinkr" )
 usethis::use_package("MODifieR")
+usethis::use_package("plotly")
+usethis::use_package("ggplot2")
+usethis::use_package("rshinyjs")
 
 ## Add modules ----
 ## Create a module infrastructure in R/
