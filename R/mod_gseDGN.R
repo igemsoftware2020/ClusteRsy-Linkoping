8883029be87ddab5440b42580501d9aa5863f511
Hyperty <56047053+Hypertyz@users.noreply.github.com>
2020-08-13 11:49:47 +0200

    UPDATE UI

--- R/mod_gseDGN.R
+++ R/mod_gseDGN.R
@@ -12,7 +12,7 @@ mod_gseDGN_ui <- function(id){
   tagList(
     uiOutput(ns("module_input")),
 
-    tags$a(class="collapsible", "Advanced settings", class = "btn btn-primary btn-block", "data-toggle" = 'collapse', "data-target" = '#advanced_enrich', "href"='#advanced_enrich',"aria-expanded" = 'false', tags$div(class= "expand_caret caret")),
+    tags$a(class="collapsible", "Advanced settings", class = "btn btn-primary btn-block", "data-toggle" = 'collapse', "data-target" = '#advanced_enrich',"aria-expanded" = 'false', tags$div(class= "expand_caret caret")),
     tags$br(),
     tags$div(id = "advanced_enrich", class = "collapse",
              tags$div(
